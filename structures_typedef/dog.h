4dd68040f546f49d571faa8700d530ae234da048
Didier Nsengiyumva <nsedidier@gmail.com>
2026-03-10 08:21:26 +0200

    Lt 8:21am

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Defines a dog's attributes.
+ * @name: Pointer to the dog's name (string).
+ * @age: The dog's age (float).
+ * @owner: Pointer to the owner's name (string).
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
